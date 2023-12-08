bd4395e50e8e742917f40dd578febdee19edc3d3
azad301016970 <150539222+azad301016970@users.noreply.github.com>
2023-12-09 01:57:06 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -59,10 +59,15 @@ run().catch(console.dir);
 
 
 app.all('/*', function (req, res, next) {
- res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   next();
 });
